2c78591859acf02392bf7c8199df88167f27befa
Pablo Castillo <psc88sdrd@gmail.com>
2023-06-28 17:14:42 -0300

    creating of category list and addition of category with button

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon ball']);
+
+  const onAddCategory = () => {
+    setCategories([...categories, 'Kimetsu no yaiba'])
+  }
+
   return (
     <>
-        <h1>Gif Search App</h1>
+      <h1>Gif Search App</h1>
+      <button onClick={ onAddCategory }>Agregar</button>
+
+      <ol>
+        {categories.map(category => {
+          return <li key={category}>{category}</li>
+        })}
+      </ol>
     </>
   )
 }
